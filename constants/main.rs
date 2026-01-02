3fcf32938de065abf8978f8cbe691fdb73230c87
unknown <anudeepgunda@gmail.com>
2026-01-01 20:48:51 -0600

    variables and constants

---
+++ constants/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut _x = 5;
+    const Y: i32 = 10;
+    println!("The Value of x is: {}", _x);
+
+    println!("The Value of Y is: {}", Y);
+
+    println!("The Value of PI is: {}", PI);
+}
+
+//one can declare the constant here with a type annotation
+const PI: f64 = 3.141592653;
---
+++ variable_mutability/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let mut _a: u16 = 5;
+    println!("The Value of a: {}", _a);
+    _a = 10;
+    println!("The Value of a: {}", _a);
+}
